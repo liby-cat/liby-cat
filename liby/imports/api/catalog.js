36dccb7b481bae30e3fe10bf6073716c7b0ae8ce
Khan(Sadh) <kmostafa@tcicredit.com>
2017-03-25 01:54:57 -0400

    first working read write

---
+++ liby/imports/api/catalog.js
@@ -0,0 +1,4 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Catalog = new Mongo.Collection('catalog');
+
--- liby/imports/components/bookList/bookList.js
+++ liby/imports/components/bookList/bookList.js
@@ -1,24 +1,35 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+
+import { Catalog } from '../../api/catalog'
+
 import template from './bookList.html';
 
 class BookListCtrl {
-  constructor() {
-    this.books = [{
-      title: 'This is task 1'
-    }, {
-      title: 'This is task 2'
-    }, {
-      title: 'This is task 3'
-    }];
+  constructor($scope) {
+    $scope.viewModel(this);
+    this.helpers({
+      books(){
+        return Catalog.find({
+          type:'book'
+        });
+      }
+    });
+  }
+  
+  addBook(newBook) {
+    newBook.type = 'book';
+    newBook.createdOn = new Date;
+    Catalog.insert(newBook);
+    // Clear form
+    this.newBook= {};
   }
 }
-
 
 export default angular.module('bookList', [
       angularMeteor
     ])
     .component('bookList', {
       templateUrl: 'imports/components/bookList/bookList.html',
-      controller: BookListCtrl
+      controller: ['$scope', BookListCtrl]
     });

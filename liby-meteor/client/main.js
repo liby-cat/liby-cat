7f0e7af69730ee9e68f9cb53dd796717635d2efb
Khan(Sadh) <kmostafa@tcicredit.com>
2017-03-29 16:59:02 -0400

    liby.cat finalizing primary framework to be loopback, removing exploratory projects

--- liby-meteor/client/main.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import bookList from '../imports/components/bookList/bookList';
-
-angular.module('liby', [
-  angularMeteor,
-    bookList.name
-]);
--- liby-meteor/imports/components/api/catalog.js
+++
@@ -1,4 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const Catalog = new Mongo.Collection('catalog');
-

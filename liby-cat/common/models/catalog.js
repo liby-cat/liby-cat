33d9af124d14fe6ce621400945c125ee80a5d3f7
nafSadh khan <khan@nafSadh.com>
2017-03-31 22:41:23 -0400

    example custom REST API entry

--- liby-cat/common/models/catalog.js
+++ liby-cat/common/models/catalog.js
@@ -1,5 +1,15 @@
 'use strict';
 
-module.exports = function(catalog) {
+module.exports = function(Catalog) {
+  Catalog.entries = function (id, cb) {
+    cb(null, {book:'khaa'});
+  };
 
+  Catalog.remoteMethod(
+    'entries',
+    {
+      accepts: {arg: 'id', type: 'string', required: true},
+      http: {path: '/:id/entries', verb: 'get'},
+      returns: {arg: 'data', type: 'Object'}
+    });
 };

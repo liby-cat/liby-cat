0203002ebca870c6b736667c184cc77f3c5eb5b4
Khan(Sadh) <kmostafa@tcicredit.com>
2017-04-03 18:32:05 -0400

    enforcing unique on catalogId, some codes for enforcing orgId fk

--- liby-cat/common/models/catalog.js
+++ liby-cat/common/models/catalog.js
@@ -1,16 +1,32 @@
 'use strict';
 
 module.exports = function(Catalog) {
-  Catalog.entries = function (id, cb) {
-    
-    cb(null, {book:'khaa'});
+  Catalog.validatesUniquenessOf('catalogId');
+
+  Catalog.observe('before save', function enforceOrgId(ctx, next) {
+    if (ctx.instance) {
+      var orgId = ctx.instance.orgId;
+      var org = ctx.instance.__get__org();
+      console.log(org);
+      console.log(orgId);
+      console.log(Catalog.app.models.Org.find);
+      Catalog.app.models.Org.find({where: {orgId: orgId}}, function(e, i) {
+        org = i;
+      });
+      console.log(org);
+    }
+    next();
+  });
+
+  Catalog.test = function(id, cb) {
+    cb(null, {book: 'khaa'});
   };
 
   Catalog.remoteMethod(
-    'entries',
+    'test',
     {
       accepts: {arg: 'id', type: 'string', required: true},
-      http: {path: '/:id/entries', verb: 'get'},
-      returns: {arg: 'data', type: 'Object'}
+      http: {path: '/:id/test', verb: 'get'},
+      returns: {arg: 'data', type: 'Object'},
     });
 };

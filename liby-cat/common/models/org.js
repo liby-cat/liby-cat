7c11d5674cc8c6f4cfa4b91e9b7496299ca34198
nafSadh khan <khan@nafSadh.com>
2017-04-10 22:29:18 -0400

    creating default org on creating on an user

--- liby-cat/common/models/org.js
+++ liby-cat/common/models/org.js
@@ -2,4 +2,10 @@
 
 module.exports = function(Org) {
   Org.validatesUniquenessOf('orgIdx');
+
+  Org.disableRemoteMethodByName('prototype.__create__admins', false);
+  Org.disableRemoteMethodByName('prototype.__delete__admins', false);
+  Org.disableRemoteMethodByName('prototype.__findById__admins', false);//GET /Org/{id}/admins/{fk}
+  Org.disableRemoteMethodByName('prototype.__updateById__admins', false);
+  Org.disableRemoteMethodByName('prototype.__destroyById__admins', false);
 };
--- liby-cat/common/models/user.js
+++ liby-cat/common/models/user.js
@@ -1,5 +1,18 @@
 'use strict';
 
-module.exports = function(User) {
+module.exports = function (user) {
+  user.validatesUniquenessOf('username');
+  user.validatesPresenceOf('username');
 
+  user.afterRemote('create', function createDefaultOrg(ctx, usr, next) {
+    console.log('user>afterRemote>create:createDefaultOrg');
+    user.app.models.Org.create({orgIdx: usr.username, title: usr.username, adminIds:[usr.id]},
+      function createUserDefaultOrg(err, obj) {
+        if(err){ next(err);}
+        if(obj){
+          console.log(obj);
+        }
+        next();
+      });
+  })
 };

b2720a6fce92106950599e05226a5d74cf844abb
Khan(Sadh) <kmostafa@tcicredit.com>
2017-04-17 10:22:22 -0400

    Added Font Awesome

--- liby-cat/client/js/app.js
+++ liby-cat/client/js/app.js
@@ -5,26 +5,30 @@ angular
   .module('app', [
     'lbServices',
     'ui.router',
-    'ngMaterial','ngAnimate','ngAria'
+    'ngMaterial', 'ngAnimate', 'ngAria'
   ])
-  .config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('landingPage', {
-        url: '',
-        templateUrl: 'views/landing-page.html',
-        controller: 'LandingPageCtrl'
-      })
-      .state('login', {
-        url: '/login',
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .state('todo', {
-        url: '/todo',
-        templateUrl: 'views/todo.html',
-        controller: 'TodoCtrl'
-      });
-    
-    $urlRouterProvider.otherwise('landingPage');
-  }]);
+  .config(['$stateProvider', '$urlRouterProvider', '$mdIconProvider',
+    function ($stateProvider, $urlRouterProvider, $mdIconProvider) {
+      $mdIconProvider
+        .defaultFontSet( 'fa' );
+      
+      $stateProvider
+        .state('landingPage', {
+          url: '',
+          templateUrl: 'views/landing-page.html',
+          controller: 'LandingPageCtrl'
+        })
+        .state('login', {
+          url: '/login',
+          templateUrl: 'views/login.html',
+          controller: 'LoginCtrl'
+        })
+        .state('todo', {
+          url: '/todo',
+          templateUrl: 'views/todo.html',
+          controller: 'TodoCtrl'
+        });
+      
+      $urlRouterProvider
+        .otherwise('landingPage');
+    }]);

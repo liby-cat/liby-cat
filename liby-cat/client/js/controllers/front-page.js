3c964641b3f796a71b7e7804ce2b438cd911f899
Khan(Sadh) <kmostafa@tcicredit.com>
2017-04-17 13:59:31 -0400

    return to front page

--- liby-cat/client/js/controllers/front-page.js
+++ liby-cat/client/js/controllers/front-page.js
@@ -2,8 +2,5 @@ angular.module('app')
   .controller('FrontPageCtrl', [
     '$scope', '$state', '$mdToast',
     function ($scope, $state, $mdToast ) {
-      $scope.goToLogin = function () {
-        $state.go('login');
-      }
     }
   ]);

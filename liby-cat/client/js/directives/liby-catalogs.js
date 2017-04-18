16de79d72eec44f2955350cebf2252e880bce9ce
Khan(Sadh) <kmostafa@tcicredit.com>
2017-04-18 17:31:37 -0400

    directive to show list of catalogs

---
+++ liby-cat/client/js/directives/liby-catalogs.js
@@ -0,0 +1,9 @@
+app.directive("libyCatalogs", function libyCatalogs() {
+  return {
+    transclude: true,
+    templateUrl: "../views/directives/liby-catalogs.html",
+    link: function link($scope, iElement, iAttrs, controller, transcludeFn) {
+    
+    }
+  };
+});

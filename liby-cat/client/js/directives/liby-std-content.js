4f01f78e61ddee2f47c070e484dd187280eb57ba
Khan(Sadh) <kmostafa@tcicredit.com>
2017-04-18 13:30:17 -0400

    Wireframe with nav and header menus as directive

---
+++ liby-cat/client/js/directives/liby-std-content.js
@@ -0,0 +1,7 @@
+app.directive("libyStdContent", function libyStdContent() {
+  return {
+    restrict: "E",
+    transclude: true,
+    templateUrl: "../views/directives/liby-std-content.html"
+  };
+});

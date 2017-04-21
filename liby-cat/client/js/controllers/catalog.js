30a17d82ee7371dca60f0d979289c3ea33a25de4
nafSadh khan <khan@nafSadh.com>
2017-04-20 23:03:32 -0400

    form to add entries

--- liby-cat/client/js/controllers/catalog.js
+++ liby-cat/client/js/controllers/catalog.js
@@ -12,13 +12,32 @@ angular.module('app')
       $scope.user = User.getCachedCurrent();
       console.log($scope.user);
       $scope.catalogId = $stateParams.id;
-      Catalog.prototype$__get__entries({id:$scope.catalogId},
+      $scope.newEntry = {}
+      
+      Catalog.prototype$__get__entries({id: $scope.catalogId},
         function success(val) {
           console.log(val);
           $scope.entries = val;
         },
         function error(er) {
         
-        });
+        }
+      );
+      
+      $scope.addNewEntry = function addNewEntry() {
+        console.log($scope.newEntry);
+        Catalog.prototype$__create__entries({id: $scope.catalogId}, $scope.newEntry,
+          function success(val) {
+            console.log(val);
+            $scope.entries.push(val);
+          }, function error(er) {
+            console.log(er);
+          }
+        );
+        $scope.newEntry = {};
+        $scope.newEntryForm.$setUntouched();
+        $scope.newEntryForm.$setPristine();
+        $scope.newEntryForm.title.$touched = false;
+      }
     }
   ]);

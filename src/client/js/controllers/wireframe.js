88e83bf7cb055a309557656a462294678ce27d05
nafSadh-khan <khan@nafsadh.com>
2017-05-01 14:43:35 -0400

    Controllers are suffixed .ctrl

--- src/client/js/controllers/wireframe.js
+++
@@ -1,8 +0,0 @@
-angular.module('app')
-  .controller('WireframeCtrl', [
-    '$scope', '$state', '$stateParams',
-    'User', 'Catalog',
-    function($scope, $state, $stateParams,
-              User, Catalog) {
-    }
-  ]);

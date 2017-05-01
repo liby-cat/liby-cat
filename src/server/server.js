ccf6578a7149d21af5a3ca78cfa1288d36a54122
nafSadh-khan <khan@nafsadh.com>
2017-05-01 18:21:33 -0400

    back-end support for pretty url

--- src/server/server.js
+++ src/server/server.js
@@ -27,8 +27,30 @@ app.start = function() {
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
   if (err) throw err;
-
   // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
 });
+
+// PRETTY URL
+// https://www.theodo.fr/blog/2017/01/pretty-url-in-angularjs-and-loopback-drop-the/
+// http://www.theodo.fr/blog/2017/01/pretty-url-in-angularjs-and-loopback-drop-the/
+var path = require('path');
+
+//List here the paths you do not want to be redirected to the angular application (scripts, stylesheets, templates, loopback REST API, ...)
+var ignoredPaths = ['/css', '/js','/lb', '/bower_components', '/views', '/api','/res'];
+
+app.all('/*', function(req, res, next) {
+  //Redirecting to index only the requests that do not start with ignored paths
+  if(!startsWith(req.url, ignoredPaths))
+    res.sendFile('index.html', { root: path.resolve(__dirname, '..', 'client') });
+  else
+    next();
+});
+
+function startsWith(string, array) {
+  for(i = 0; i < array.length; i++)
+    if(string.startsWith(array[i]))
+      return true;
+  return false;
+}

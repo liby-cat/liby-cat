a9f0e2098685257b236c6ababb8259bbd4402708
nafSadh-khan <khan@nafsadh.com>
2017-05-03 12:35:37 -0400

    gen cmd with git patch

--- scripts/gen-lb-services.cmd
+++ scripts/gen-lb-services.cmd
@@ -1,1 +1,1 @@
-cd src && lb-ng ./server/server.js ./client/lb/lb-services.js && cd ..
+cd src && lb-ng ./server/server.js ./client/lb/lb-services.js && cd .. &&  echo 'patching' && git apply ./patches/lb-services-patch.diff &&  echo 'patched'

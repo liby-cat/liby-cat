ad88600d2d93252fe0f90f3c963673b1c88e2109
nafSadh <khan@nafSadh.com>
2017-05-12 12:04:38 -0400

    Icons attributing frameworks

--- src/client/js/icons.js
+++ src/client/js/icons.js
@@ -2,13 +2,17 @@ app.config(['$mdIconProvider', function appIcons($mdIconProvider) {
   $mdIconProvider
     .icon('logo', '../res/logo.svg')
     .icon('logo-icon', '../res/icons/logo-icon.svg')
-    .icon('wiki_books', '../res/icons/Books.svg')
-    .icon('pile_books', '../res/icons/pile-books.svg')
+    .icon('books-pile', '../res/icons/pile-books.svg')
+    .icon('books-wiki', '../res/icons/Books.svg')
     .icon('crown-azure', '../res/icons/crown-azure.svg')
     .icon('crown-gold', '../res/icons/crown-gold.svg')
     .icon('crown-sharp', '../res/icons/crown-sharp.svg')
     .icon('crown-solid', '../res/icons/crown-solid.svg')
     .icon('lib-case', '../res/icons/lib-case.svg')
     .icon('list', '../res/icons/list.svg')
+    .icon('nodejs', '../res/icons/nodejs.svg')
+    .icon('loopback', '../res/icons/loopback.svg')
+    .icon('mongodb', '../res/icons/mongodb.svg')
+    .icon('angular-js', '../res/icons/angular-js.svg')
   ;
 }]);

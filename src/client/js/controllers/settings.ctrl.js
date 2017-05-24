dfe6a60933d33c42120d4bb0b287d45f64f62010
nafSadh <khan@nafSadh.com>
2017-05-24 16:47:18 -0400

    User profile UI

--- src/client/js/controllers/settings.ctrl.js
+++ src/client/js/controllers/settings.ctrl.js
@@ -1,8 +1,24 @@
 app.controller('SettingsCtrl', [
-    '$scope', '$state', '$stateParams',
-    'User',
-    function($scope, $state, $stateParams,
-              User) {
-      $scope.user = $scope.user ? $scope.user : User.getCachedCurrent();
-    }
-  ]);
+  '$scope', '$state', '$stateParams', '$mdDialog',
+  'User',
+  function ($scope, $state, $stateParams, $mdDialog,
+            User) {
+    $scope.user = $scope.user ? $scope.user : User.getCachedCurrent();
+    
+    $scope.editNamePrompt = function (ev) {
+      // Appending dialog to document.body to cover sidenav in docs app
+      var confirm = $mdDialog.prompt()
+        .title('Edit name')
+        .placeholder($scope.user.name ? $scope.user.name : 'Name')
+        .initialValue($scope.user.name)
+        .targetEvent(ev)
+        .ok('Update')
+        .cancel('Cancel');
+      
+      $mdDialog.show(confirm).then(function (result) {
+        $scope.user.name = result;
+      }, function () {
+      });
+    };
+  }
+]);
